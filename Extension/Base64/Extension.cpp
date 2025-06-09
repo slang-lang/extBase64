40f2e578ffbdc2efcd3193f75cceb38912087efa
madelmann <madelmann@users.noreply.github.com>
2025-06-09 23:25:37 +0200

    changed version number to 0.1.3

--- Extension/Base64/Extension.cpp
+++ Extension/Base64/Extension.cpp
@@ -13,33 +13,33 @@
 namespace base64 {
 
 Extension::Extension()
-: AExtension( "extBase64", "0.1.2" )
+: AExtension( "extBase64", "0.1.3" )
 {
     mName = "extBase64 (using base64 2.rc.09)";
 }
 
 void Extension::initialize( Slang::Extensions::ExtensionNamespace *scope )
 {
-    ( void )scope;
+    (void) scope;
 
     //auto* decode = new base64_decode( scope );
     //auto* encode = new base64_encode( scope );
-    //
+
     //scope->defineMethod( decode->getName(), decode );
     //scope->defineMethod( encode->getName(), encode );
 }
 
 void Extension::provideMethods( Slang::Extensions::ExtensionMethods &methods )
 {
-  ( void )methods;
+    (void) methods;
 
-  methods.push_back( new base64_decode() );
-  methods.push_back( new base64_encode() );
+    methods.push_back( new base64_decode() );
+    methods.push_back( new base64_encode() );
 }
 
 } // namespace base64
 
 extern "C" Slang::Extensions::AExtension *factory(void)
 {
-  return dynamic_cast<Slang::Extensions::AExtension *>( new base64::Extension() );
+    return dynamic_cast<Slang::Extensions::AExtension *>( new base64::Extension() );
 }
